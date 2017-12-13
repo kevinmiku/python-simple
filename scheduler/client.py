40b8e9dbe31b2712e471c5d601a113b82f88ea36
hexie <hexie@baidu.com>
2017-12-13 16:51:58 +0800

    modify server and client

--- scheduler/client.py
+++ scheduler/client.py
@@ -18,9 +18,10 @@ class Client(object):
 
     def start_client(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((config.Server.servertHostName, config.Server.port2))
         pass
 
-    def send_request(self, request): #python2
+    def send_request(self, request):
         try:
             print((config.Server.HOST_IP, config.Server.port))
             self.client.connect((config.Server.HOST_IP, config.Server.port))
@@ -34,20 +35,20 @@ class Client(object):
             return -1
         return 1
 
-    def send_requestB(self, request): #python3
-        try:
-            self.client.connect((config.Server.HOST_IP, config.Server.port))
-            msg = self.client.recv(1024)
-            self.client.close()
-            print(msg.decode('utf-8'))
-        except BaseException:
-            print("errer")
-
+    def send_requestC(self):
+        while True:
+            send_data = input("输入发送内容:")
+            self.client.sendall(bytes(send_data, encoding="utf8"))
+            if send_data == "byebye":
+                break
+            accept_data = str(self.client.recv(1024), encoding="utf8")
+            print("".join(("接收内容：", accept_data)))
+        self.client.close()
 
 if __name__ == '__main__':
     c = Client()
     #request = {'type':'Email', 'content': '我就是条邮件', 'send_time': '2017-12-07 16:44'}
-    message = "GET / HTTP/1.1\r\n\r\n"
+    # message = "GET / HTTP/1.1\r\n\r\n"
     #remote_ip = socket.gethostbyname(host)
     c.start_client()
-    c.send_requestB(message)
+    c.send_requestC()
---
+++ scheduler/server.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+from scheduler import config
+import socket
+import socketserver
+
+class Server:
+    def __init__(self):
+        """
+        init function
+        :return:
+        """
+        self.server = None
+
+    def start(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((config.Server.servertHostName, config.Server.port2)) #绑定端口
+        self.server.listen(5)
+
+    def listen(self):
+        while True:
+            conn, addr = self.server.accept()
+            while True:
+                accept_data = str(conn.recv(1024), encoding="utf8")
+                print("".join(["接收内容：", accept_data, "     客户端口：", str(addr[1])]))
+                if accept_data == "byebye":  # 如果接收到“byebye”则跳出循环结束和第一个客户端的通讯，开始与下一个客户端进行通讯
+                    break
+                send_data = input("输入发送内p容：")
+                conn.sendall(bytes(send_data, encoding="utf8"))
+            conn.close()  # 跳出循环时结束通讯
+
+
+if __name__ == '__main__':
+    server = Server()
+    server.start()
+    server.listen()
